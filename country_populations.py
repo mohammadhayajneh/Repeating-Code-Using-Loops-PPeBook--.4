3192d1c663faf511287cec48d10516a057099351
Mohammad Hayajneh <35841393+mohammadhayajneh@users.noreply.github.com>
2018-02-17 10:37:54 -0500

    Add files via upload

---
+++ country_populations.py
@@ -0,0 +1,11 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> country_populations=[1295,23,7, 3, 47, 21]
+>>> total=0
+>>> for population in country_populations:
+	total+=population
+
+	
+>>> print (total)
+1396
+>>> 
